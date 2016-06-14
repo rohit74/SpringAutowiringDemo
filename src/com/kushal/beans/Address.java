7215364b005205069c87136bd3f439880382aec0
Rohit Wagle <rohit74@tamu.edu>
2016-06-14 12:36:51 -0500

    done

---
+++ src/com/kushal/beans/Address.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+/**
+ * @author rohit
+ *
+ */
+package com.kushal.beans;
+
+public class Address{
+	public Address(){
+		System.out.println("Inside address Constructor");
+	}
+	public void display(){
+		System.out.println("address");
+	}
+}
